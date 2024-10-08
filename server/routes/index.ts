f06037219e300694f1a5b7eceb5ea5f8708c13b2
rossdowthwaite <rossdowthwaite@gmail.com>
2024-10-08 13:24:30 +0100

    bump lib

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -43,5 +43,6 @@ export default function routes(services: Services): Router {
   addBookmarkingRoutes(libRouteParams)
   addDashboardRoutes(libRouteParams)
 
+  // // // // // //
   return router
 }
