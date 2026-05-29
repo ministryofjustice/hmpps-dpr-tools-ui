56a982d9b6cbeb88e99a2ec44c96b15cf571e953
rossdowthwaite <rossdowthwaite@gmail.com>
2026-05-29 10:04:07 +0100

    Fix sec issue with deps

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,9 +2,10 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
-    'node_modules/cypress@14.3.3': 'ALLOW',
+    'node_modules/cypress@14.5.4': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'FORBID',
     'node_modules/fsevents@2.3.3': 'FORBID',
-    'node_modules/esbuild@0.25.8': 'ALLOW'
+    'node_modules/esbuild@0.25.8': 'ALLOW',
+    'node_modules/vite/node_modules/esbuild@0.27.7': 'ALLOW'
   },
 })
