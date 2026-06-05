46074ac682eb69eec7cbbfb0bce97274f14e77e8
Ross Dowthwaite <rossdowthwaite@gmail.com>
2026-06-05 14:17:04 +0100

    Dpr2 update libs and sec fixes (#707)
    * npm i
    
    * Bump Glob to 11.1.0
    
    * Bump UUID to 14.0.0
    
    * Bump Cypress to 15.16.0
    
    * bump jest-junit 17
    
    * fix serialize-javascript
    
    * Fix deps
    
    * updated prettier
    
    * updated helmet
    
    ---------
    
    Co-authored-by: Sunil <skhutan@users.noreply.github.com>

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,7 +2,7 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
-    'node_modules/cypress@14.3.3': 'ALLOW',
+    'node_modules/cypress@15.16.0': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'FORBID',
     'node_modules/fsevents@2.3.3': 'FORBID',
     'node_modules/esbuild@0.25.8': 'ALLOW',
