95cfb88848160cd54cac95af0d1485fd4d689cb7
Ross Dowthwaite <rossdowthwaite@gmail.com>
2026-06-23 12:05:32 +0100

    Revert app insights as its caused high level vulnerabilies due to protobuf (#763)

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,11 +2,9 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
-    "node_modules/@grpc/proto-loader/node_modules/protobufjs@7.6.4": "FORBID",
     'node_modules/cypress@15.17.0': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'FORBID',
     'node_modules/fsevents@2.3.3': 'FORBID',
     'node_modules/esbuild@0.28.1': 'ALLOW',
-    "node_modules/protobufjs@8.0.1": "FORBID"
   },
 })
