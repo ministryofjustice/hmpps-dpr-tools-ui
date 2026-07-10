f13b5401e7fd0230ed29cc382c878a284125638b
James Reed <james.reed1@justice.gov.uk>
2026-07-10 12:44:16 +0100

    removing tests for stuff we dont have yet, fixing more config

--- cypress-tests/common/pages/index.ts
+++ cypress-tests/common/pages/index.ts
@@ -2,7 +2,8 @@ import Page, { PageElement } from './page'
 
 export default class IndexPage extends Page {
   constructor() {
-    super('Digital Prison Reporting')
+    cy.visit('/preview')
+    super('DPR: Preview Reports')
   }
 
   headerUserName = (): PageElement => cy.get('[data-qa=header-user-name]')
