049d3944e8e31dd3f08c06871cbe8f2c62814c30
James Reed <109068236+ReedSoftware@users.noreply.github.com>
2026-07-28 08:32:10 +0100

    Tools UI probation (#807)
    * tools ui helm files and int and acceptance tests
    
    * add missing build info record step
    
    * add build info script
    
    * fix tests
    
    * remove old tests, update config
    
    * fix types and tsconfig
    
    * removing tests for stuff we dont have yet, fixing more config
    
    * populate authsource to user
    
    * missing env var
    
    * noop for ci
    
    * fix token refs in config
    
    * stop trying to deploy probation for now
    
    * revert: stop trying to deploy probation for now
    
    * wrong context name
    
    * typos
    
    * fix secret name
    
    * need productid
    
    * add dps_url
    
    * require probation and prisons deployed before acceptance
    
    * fix uris
    
    * change dnsnames
    
    * noop for ci
    
    * fix cci pipeline
    
    * fix cci pipeline
    
    * fix cci pipeline
    
    * fix cci pipeline
    
    * revert previous changes and just delete probation dev remember
    
    * revert to original and build from there instead of copying
    
    * execute on branch too
    
    * add probation app and typecheck
    
    * update context names
    
    * use auth token temporarily
    
    * use api system token token temporarily
    
    * revert token changes
    
    * log system token
    
    * change to updated dns
    
    * put required source in main values, update probation to have external user auth
    
    * try internal dns
    
    * fix helm config, improve logs
    
    * remove branch from allowed branches
    
    * linting

---
+++ cypress-tests/common/pages/index.ts
@@ -0,0 +1,14 @@
+import Page, { PageElement } from './page'
+
+export default class IndexPage extends Page {
+  constructor() {
+    cy.visit('/preview')
+    super('DPR: Preview Reports')
+  }
+
+  headerUserName = (): PageElement => cy.get('[data-qa=header-user-name]')
+
+  reportLinks = (): PageElement => cy.get('a[href^="/dpr/request-report"]')
+
+  reportTable = (): PageElement => cy.get('.dpr-search-table')
+}
---
+++ cypress-tests/integration-tests/pages/index.ts
@@ -0,0 +1,13 @@
+import Page, { PageElement } from './page'
+
+export default class IndexPage extends Page {
+  constructor() {
+    super('DPR Tools')
+  }
+
+  headerUserName = (): PageElement => cy.get('[data-qa=header-user-name]')
+
+  headerPhaseBanner = (): PageElement => cy.get('[data-qa=header-phase-banner]')
+
+  previewReportCardLink = (): PageElement => cy.get('[href="/preview"]')
+}
